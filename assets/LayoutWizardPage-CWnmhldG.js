178765454f89f94d2df35b40d27db21d8b03ff2c
Opus Architect <opus@pbscoutpro.dev>
2026-04-14 09:49:52 +0000

    Updates

--- assets/LayoutWizardPage-CWnmhldG.js
+++ assets/LayoutWizardPage-CWnmhldG.js
@@ -1,4 +1,4 @@
-import{d as S,j as e,e as s,C as o,f as u,F as g,B as j,R as w,I as W,al as C,T as B,u as P,b as G,r as H,am as I,i as D,k as U,l as R,m as Y}from"./index-CF0PDo1L.js";import{P as _}from"./PageHeader-zXlnjf0d.js";import{g as V,u as F,y as K,e as X}from"./helpers-BAE_qoQ6.js";import{C as J}from"./CalibrationView-DYkXjGKY.js";function Q(t,a){return`You are analyzing a paintball field layout image. Identify INDIVIDUAL inflatable bunkers.
+import{d as S,j as e,e as s,C as o,f as u,F as g,B as j,R as w,I as W,al as C,T as B,u as P,b as G,r as H,am as I,i as D,k as U,l as R,m as Y}from"./index-nfVLeNM2.js";import{P as _}from"./PageHeader-Dk5VUFiY.js";import{g as V,u as F,y as K,e as X}from"./helpers-BAE_qoQ6.js";import{C as J}from"./CalibrationView-CEtHOQsL.js";function Q(t,a){return`You are analyzing a paintball field layout image. Identify INDIVIDUAL inflatable bunkers.
 
 CRITICAL: Each inflatable = one bunker. Bunkers placed next to each other form structures (like "snake") but each piece is SEPARATE. Break down clusters into individual inflatables.
 
